c94594dfaae527c3a57d8a895f72acfbd6f9f471
amulyamari <amulya@saimail.com>
2018-07-25 15:44:11 +0000

    Done.

---
+++ arrays.rb
@@ -0,0 +1,11 @@
+def change_rainbow_arrays
+  rainbow_colors=["yellow", "default", "light_cyan"]
+  rainbow_colors[0]="light_yellow"
+  rainbow_colors[1]="light_red"
+  rainbow_colors[2]="red"
+  rainbow_colors.push ("green")
+  rainbow_colors.push("blue")
+  puts rainbow_colors
+end
+change_rainbow_arrays
+ 
